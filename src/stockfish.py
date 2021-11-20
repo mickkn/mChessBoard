30c100dc5c5365a45160261fd2f361c8c907bfd9
Mick Kirkegaard <mickkn@gmail.com>
2021-11-20 14:52:25 +0000

    Updated stockfish

--- src/stockfish.py
+++ src/stockfish.py
@@ -7,21 +7,7 @@
 
 import subprocess
 from typing import Any, List, Optional
-
-DEFAULT_STOCKFISH_PARAMS = {
-    "Write Debug Log": "false",
-    "Contempt": 0,
-    "Min Split Depth": 0,
-    "Threads": 1,
-    "Ponder": "false",
-    "Hash": 16,
-    "MultiPV": 1,
-    "Skill Level": 20,
-    "Move Overhead": 30,
-    "Minimum Thinking Time": 20,
-    "Slow Mover": 80,
-    "UCI_Chess960": "false",
-}
+import copy
 
 
 class Stockfish:
@@ -30,11 +16,36 @@ class Stockfish:
     def __init__(
         self, path: str = "stockfish", depth: int = 2, parameters: dict = None
     ) -> None:
-        self.stockfish = subprocess.Popen(
-            path, universal_newlines=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE
-        )
+        self.default_stockfish_params = {
+            "Write Debug Log": "false",
+            "Contempt": 0,
+            "Min Split Depth": 0,
+            "Threads": 1,
+            "Ponder": "false",
+            "Hash": 16,
+            "MultiPV": 1,
+            "Skill Level": 20,
+            "Move Overhead": 30,
+            "Minimum Thinking Time": 20,
+            "Slow Mover": 80,
+            "UCI_Chess960": "false",
+            "UCI_LimitStrength": "false",
+            "UCI_Elo": 1350,
+        }
+        try:
+            self.stockfish = subprocess.Popen(
+                path, universal_newlines=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE
+            )
+        except PermissionError as err:
+            print(err)
+            print("Try 'chmod +x' your engine")
 
-        #self._stockfish_major_version: int = int(self._read_line().split(" ")[1])
+        """
+        # This version stuff is not working when using other engines
+        self._stockfish_major_version: int = int(
+            self._read_line().split(" ")[1].split(".")[0]
+        )
+        """
 
         self._put("uci")
 
@@ -43,12 +54,12 @@ class Stockfish:
 
         if parameters is None:
             parameters = {}
-        self._parameters = DEFAULT_STOCKFISH_PARAMS
+        self._parameters = copy.deepcopy(self.default_stockfish_params)
         self._parameters.update(parameters)
         for name, value in list(self._parameters.items()):
             self._set_option(name, value)
 
-        self._start_new_game()
+        self._prepare_for_new_position(True)
 
     def get_parameters(self) -> dict:
         """Returns current board position.
@@ -58,8 +69,19 @@ class Stockfish:
         """
         return self._parameters
 
-    def _start_new_game(self) -> None:
-        self._put("ucinewgame")
+    def reset_parameters(self) -> None:
+        """Resets the stockfish parameters.
+
+        Returns:
+            None
+        """
+        self._parameters = copy.deepcopy(self.default_stockfish_params)
+        for name, value in list(self._parameters.items()):
+            self._set_option(name, value)
+
+    def _prepare_for_new_position(self, send_ucinewgame_token: bool = True) -> None:
+        if send_ucinewgame_token:
+            self._put("ucinewgame")
         self._is_ready()
         self.info = ""
 
@@ -106,10 +128,28 @@ class Stockfish:
               Must be in full algebraic notation.
               example: ['e2e4', 'e7e5']
         """
-        self._start_new_game()
+        self._prepare_for_new_position(True)
         if moves is None:
             moves = []
         self._put(f"position startpos moves {self._convert_move_list_to_str(moves)}")
+
+    def make_moves_from_current_position(self, moves: List[str]) -> None:
+        """Sets a new position by playing the moves from the current position.
+
+        Args:
+            moves:
+              A list of moves to play in the current position, in order to reach a new position.
+              Must be in full algebraic notation.
+              Example: ["g4d7", "a8b8", "f1d1"]
+        """
+        if moves == []:
+            raise ValueError(
+                "No moves sent in to the make_moves_from_current_position function."
+            )
+        self._prepare_for_new_position(False)
+        self._put(
+            f"position fen {self.get_fen_position()} moves {self._convert_move_list_to_str(moves)}"
+        )
 
     def get_board_visual(self) -> str:
         """Returns a visual representation of the current board position.
@@ -125,6 +165,9 @@ class Stockfish:
             if "+" in board_str or "|" in board_str:
                 count_lines += 1
                 board_rep += f"{board_str}\n"
+        if self._stockfish_major_version >= 12:
+            board_str = self._read_line()
+            board_rep += f"  {board_str}\n"
         return board_rep
 
     def get_fen_position(self) -> str:
@@ -150,20 +193,41 @@ class Stockfish:
         Returns:
             None
         """
+        self._set_option("UCI_LimitStrength", "false")
         self._set_option("Skill Level", skill_level)
         self._parameters.update({"Skill Level": skill_level})
 
-    def set_fen_position(self, fen_position: str) -> None:
+    def set_elo_rating(self, elo_rating: int = 1350) -> None:
+        """Sets current elo rating of stockfish engine, ignoring skill level.
+
+        Args:
+            elo_rating: Aim for an engine strength of the given Elo
+
+        Returns:
+            None
+        """
+        self._set_option("UCI_LimitStrength", "true")
+        self._set_option("UCI_Elo", elo_rating)
+        self._parameters.update({"UCI_Elo": elo_rating})
+
+    def set_fen_position(
+        self, fen_position: str, send_ucinewgame_token: bool = True
+    ) -> None:
         """Sets current board position in Forsyth–Edwards notation (FEN).
 
         Args:
             fen_position:
               FEN string of board position.
 
+            send_ucinewgame_token:
+              Whether to send the "ucinewgame" token to the Stockfish engine.
+              The most prominent effect this will have is clearing Stockfish's transposition table,
+              which should be done if the new position is unrelated to the current position.
+
         Returns:
             None
         """
-        self._start_new_game()
+        self._prepare_for_new_position(send_ucinewgame_token)
         self._put(f"position fen {fen_position}")
 
     def get_best_move(self) -> Optional[str]:
@@ -253,6 +317,75 @@ class Stockfish:
                         }
             elif splitted_text[0] == "bestmove":
                 return evaluation
+
+    def get_top_moves(self, num_top_moves: int = 5) -> List[dict]:
+        """Returns info on the top moves in the position.
+
+        Args:
+            num_top_moves:
+                The number of moves to return info on, assuming there are at least
+                those many legal moves.
+
+        Returns:
+            A list of dictionaries. In each dictionary, there are keys for Move, Centipawn, and Mate;
+            the corresponding value for either the Centipawn or Mate key will be None.
+            If there are no moves in the position, an empty list is returned.
+        """
+
+        if num_top_moves <= 0:
+            raise ValueError("num_top_moves is not a positive number.")
+        old_MultiPV_value = self._parameters["MultiPV"]
+        if num_top_moves != self._parameters["MultiPV"]:
+            self._set_option("MultiPV", num_top_moves)
+            self._parameters.update({"MultiPV": num_top_moves})
+        self._go()
+        lines = []
+        while True:
+            text = self._read_line()
+            splitted_text = text.split(" ")
+            lines.append(splitted_text)
+            if splitted_text[0] == "bestmove":
+                break
+        top_moves: List[dict] = []
+        multiplier = 1 if ("w" in self.get_fen_position()) else -1
+        for current_line in reversed(lines):
+            if current_line[0] == "bestmove":
+                if current_line[1] == "(none)":
+                    top_moves = []
+                    break
+            elif (
+                ("multipv" in current_line)
+                and ("depth" in current_line)
+                and current_line[current_line.index("depth") + 1] == self.depth
+            ):
+                multiPV_number = int(current_line[current_line.index("multipv") + 1])
+                if multiPV_number <= num_top_moves:
+                    has_centipawn_value = "cp" in current_line
+                    has_mate_value = "mate" in current_line
+                    if has_centipawn_value == has_mate_value:
+                        raise RuntimeError(
+                            "Having a centipawn value and mate value should be mutually exclusive."
+                        )
+                    top_moves.insert(
+                        0,
+                        {
+                            "Move": current_line[current_line.index("pv") + 1],
+                            "Centipawn": int(current_line[current_line.index("cp") + 1])
+                            * multiplier
+                            if has_centipawn_value
+                            else None,
+                            "Mate": int(current_line[current_line.index("mate") + 1])
+                            * multiplier
+                            if has_mate_value
+                            else None,
+                        },
+                    )
+            else:
+                break
+        if old_MultiPV_value != self._parameters["MultiPV"]:
+            self._set_option("MultiPV", old_MultiPV_value)
+            self._parameters.update({"MultiPV": old_MultiPV_value})
+        return top_moves
 
     def set_depth(self, depth_value: int = 2) -> None:
         """Sets current depth of stockfish engine.
